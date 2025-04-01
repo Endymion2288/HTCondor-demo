eca80a8a6d9d781bef5e164bf9af03cf25fc8208
Chi Wang <chiw@lxplus909.cern.ch>
2025-04-01 18:52:40 +0200

    :zap: Improved reliability by updating eos path format.
    :art: Adopted '/eos/user/c/chiw' instead of '/eos/home-c/chiw'. In
    accordance with CERN KB0004244. Reliability improved from ~20% to ~100%.

--- make_dirlist.sh
+++ make_dirlist.sh
@@ -5,7 +5,7 @@
 input_dirs_regex=".*triOniaVtxValid.*202[2-4].*\/[0-9]\{4\}"
 
 # 查找输入文件并生成清单
-find /eos/home-c/chiw/JpsiJpsiUps/rootNtuple/ParkingDoubleMuonLowMass[0-7] \
+find /eos/user/c/chiw/JpsiJpsiUps/rootNtuple/ParkingDoubleMuonLowMass[0-7] \
      -type d -regextype 'posix-basic' -regex "$input_dirs_regex" > datadir.list
 
 echo "Generated dataset list with $(wc -l < datadir.list) entries"
